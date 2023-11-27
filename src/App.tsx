00c64c69a9c651787e644f0cba04a3abb071d533
MarlonChi <marlonchiodelli@hotmail.com>
2023-11-27 14:32:16 -0300

    feat: Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/defaults";
 import { GlobalStyle } from "./styles/global";
+import { Transactions } from "./pages/Transactions";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>DT Money</h1>
+      <Transactions />
       <GlobalStyle />
     </ThemeProvider>
   );
