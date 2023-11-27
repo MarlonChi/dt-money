0a0cea5315af768cba808ae4e9fc7815e5e4c087
MarlonChi <marlonchiodelli@hotmail.com>
2023-11-27 14:20:08 -0300

    feat: setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/defaults";
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
-  return <h1>DT Money</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>DT Money</h1>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
